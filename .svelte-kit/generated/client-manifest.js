62632a606473b2d2aa84835f0c905972175248d6
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2022-09-19 03:18:55 +0300

    YES

---
+++ .svelte-kit/generated/client-manifest.js
@@ -0,0 +1,21 @@
+export { matchers } from './client-matchers.js';
+
+			export const nodes = [() => import('./nodes/0'),
+	() => import('./nodes/1'),
+	() => import('./nodes/2'),
+	() => import('./nodes/3'),
+	() => import('./nodes/4'),
+	() => import('./nodes/5')];
+
+			export const server_loads = [];
+
+			export const dictionary = {
+	"": [2],
+	"links": [3],
+	"plugins": [4],
+	"projects": [5]
+};
+
+			export const hooks = {
+				handleError: (({ error }) => { console.error(error); return { message: 'Internal Error' }; }),
+			};
