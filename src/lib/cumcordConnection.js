58e927dc9ab5eca72613f8a4d78ad5d33d3719f4
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2022-09-19 14:28:42 +0300

    Update cumcordConnection.js

--- src/lib/cumcordConnection.js
+++ src/lib/cumcordConnection.js
@@ -1,11 +1,10 @@
 /* eslint-disable no-async-promise-executor */
-let ws;
 
-export function init() {
+export function getConnection() {
   return new Promise(async r => {
-    if (ws) return ws;
-
+    let ws;
     let count = 0;
+    
     for (let i = 0; i < 11; i++) {
       try {
         let s = new WebSocket(`ws://127.0.0.1:${6463 + i}/cumcord`);
@@ -17,14 +16,13 @@ export function init() {
         s.onerror = () => {
           count++;
         }
-      // eslint-disable-next-line no-empty
+        // eslint-disable-next-line no-empty
       } catch { };
     }
-
     
     // eslint-disable-next-line no-constant-condition
     while (true) {
-      if (count >= 11) break;
+      if (count >= 10) break;
       await new Promise(r => setTimeout(r, 100));
     }
 
@@ -33,11 +31,12 @@ export function init() {
 }
 
 export async function installPlugin(url) {
-  let sock = await init();
+  let sock = await getConnection();
   if (!sock) return alert("Unable to find a Cumcord instance on your computer, please first start the Cumcord.");
 
   sock?.send(JSON.stringify({
     action: "INSTALL_PLUGIN",
     url
-  }))
+  }));
+  sock?.close();
 }
